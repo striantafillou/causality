d4e0364bb4ce90c9035546605d95612002a2bc2a
sosata <sosata@gmail.com>
2017-02-07 11:40:43 -0600

    bug fix

--- read_data.py
+++ read_data.py
@@ -1,7 +1,7 @@
 
 # coding: utf-8
 
-# In[122]:
+# In[ ]:
 
 import os
 import pickle
@@ -9,9 +9,9 @@ import pandas as pd
 import numpy as np
 import datetime, time
 
-data_dir = '/home/sohrob/Dropbox/Data/CS120/'
+data_dir = '/data/CS120/'
 subjects = os.listdir(data_dir)
-subjects.remove('.dropbox')
+# subjects.remove('.dropbox')
 # subjects = subjects[:2]
 
 timezones = pd.read_csv('../CS120/general/timezones.csv',sep='\t',header=None)
@@ -24,45 +24,44 @@ emss = []
 
 for subj in subjects:
     
-    print subj,
+    print subj
     
     act = pd.DataFrame(columns=['date','act'])
     if os.path.exists(data_dir+subj+'/act.csv'):
-        data_act = pd.read_csv(data_dir+subj+'/act.csv',sep='\t',header=None)
+        data = pd.read_csv(data_dir+subj+'/act.csv',sep='\t',header=None)
         # convert timestamps to daystamps
-        data_act[0] = np.floor((data_act[0]+ 3600*float(timezones.loc[timezones[0]==subj,1]))/86400.0)
+        data[0] = np.floor((data[0]+ 3600*float(timezones.loc[timezones[0]==subj,1]))/86400.0)
         # loading into new matrix
-        act['date'] = np.arange(data_act.loc[0,0],data_act.loc[data_act.shape[0]-1,0])
+        act['date'] = np.arange(data.loc[0,0],data.loc[data.shape[0]-1,0])
         for (i,da) in enumerate(act['date']):
-             act.loc[i,'act'] = np.sum(data_act.loc[data_act[0]==da,1]=='BIKING')+np.sum(data_act.loc[data_act[0]==da,1]=='ON_FOOT')
+             act.loc[i,'act'] = np.sum(data.loc[data[0]==da,1]=='BIKING')+np.sum(data.loc[data[0]==da,1]=='ON_FOOT')
     else:
         print ' no act data'
     acts.append(act)
 
 
+    ema = pd.DataFrame(columns=['date','stress','mood','energy','focus'])
     if os.path.exists(data_dir+subj+'/emm.csv'):
         data = pd.read_csv(data_dir+subj+'/emm.csv',sep='\t',header=None)
         # convert timestamps to daystamps
         data[0] = np.floor((data[0]+ 3600*float(timezones.loc[timezones[0]==subj,1]))/86400.0)
         # loading into new matrix
-        ema = pd.DataFrame(columns=['date','stress','mood','energy','focus'])
         ema['date'] = np.arange(data.loc[0,0],data.loc[data.shape[0]-1,0])
         for (i,da) in enumerate(ema['date']):
             ema.loc[i,'stress'] = np.nanmean(data.loc[data[0]==da,1])
             ema.loc[i,'mood'] = np.nanmean(data.loc[data[0]==da,2])
             ema.loc[i,'energy'] = np.nanmean(data.loc[data[0]==da,3])
             ema.loc[i,'focus'] = np.nanmean(data.loc[data[0]==da,4])
-        emas.append(ema)
     else:
         print ' no ema data'
-        emas.append([])
+    emas.append(ema)
         
+    ems = pd.DataFrame(columns=['date','duration','quality','daytype'])
     if os.path.exists(data_dir+subj+'/ems.csv'):
         data = pd.read_csv(data_dir+subj+'/ems.csv',sep='\t',header=None)
         # convert timestamps to daystamps
         data[0] = np.floor((data[3]/1000.0+3600*float(timezones.loc[timezones[0]==subj,1]))/86400.0)
         # loading into new matrix
-        ems = pd.DataFrame(columns=['date','duration','quality','daytype'])
         ems['date'] = np.arange(data.loc[0,0],data.loc[data.shape[0]-1,0])
         for (i,da) in enumerate(ems['date']):
             ems.loc[i,'duration'] = np.nanmean(data.loc[data[0]==da,3]-data.loc[data[0]==da,2])/1000.0
@@ -71,10 +70,9 @@ for subj in subjects:
                 ems.loc[i,'daytype'] = data.loc[data[0]==da,6].values[0]
             else:
                 ems.loc[i,'daytype'] = np.nan
-        emss.append(ems)
     else:
         print ' no ems data'
-        emss.append([])
+    emss.append(ems)
 
 # aligning the data
 data = []
@@ -95,4 +93,13 @@ for (i,_) in enumerate(subjects):
     a = pd.merge(a,acts[i],on='date',how='outer')
     
     data.append(a)
+    
+with open('data.dat','w') as f:
+    pickle.dump(data, f)
+f.close()
+
+
+# In[ ]:
+
+i
 
