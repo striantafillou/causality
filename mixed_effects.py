a506474fddf68cd230162c157561023f4612a90e
sosata <sosata@gmail.com>
2017-02-15 12:04:30 -0600

    fix mixed_effects

--- mixed_effects.py
+++ mixed_effects.py
@@ -1,7 +1,7 @@
 
 # coding: utf-8
 
-# In[82]:
+# In[101]:
 
 import statsmodels.api as sm 
 import statsmodels.formula.api as smf
@@ -15,19 +15,7 @@ with open('data.dat') as f:
     data = pickle.load(f)
 f.close()
 
-# removing nan rows
-for i in range(len(data)):
-    data[i] = data[i].dropna()
-    data[i] = data[i].reset_index(drop=True)
-
-# removing empty subjects
-ind_empty = []
-for i in range(len(data)):
-    if data[i].shape[0]==0:
-        ind_empty.append(i)
-data = [i for j, i in enumerate(data) if j not in ind_empty]
-print str(len(ind_empty))+' subjects removed due to lack of data'
-
+# adding subject ids
 for i in range(len(data)):
     data[i]['subject'] = pd.Series(i+np.zeros(data[i].shape[0]), index=data[i].index, dtype=int)
 
@@ -35,28 +23,26 @@ for i in range(len(data)):
 data = pd.concat(data, axis=0)
 data = data.reset_index(drop=True)
 
-# remove extra columns
-# data = data.loc[:,['subject','mood','quality']]
-data['mood'] = data['mood'].astype(float)
-data['mood_prev'] = data['mood_prev'].astype(float)
-data['quality'] = data['quality'].astype(float)
+# keeping only relevant variables
+data = data[['subject','mood','quality','mood_prev']]
+
+# removing nan rows
+data = data.dropna()
+data = data.reset_index(drop=True)
 
 md = smf.mixedlm('mood ~ quality', data, groups=data['subject'])
 mdf = md.fit() 
 print(mdf.summary())
 
 
-# In[83]:
+# In[102]:
 
 md = smf.mixedlm('quality ~ mood_prev', data, groups=data['subject'])
 mdf = md.fit() 
 print(mdf.summary())
 
 
-# In[60]:
+# In[103]:
 
-data = sm.datasets.get_rdataset('dietox', 'geepack').data
-md = smf.mixedlm('Weight ~ Time', data, groups=data['Pig'])
-mdf = md.fit() 
-print(mdf.summary())
+data
 
