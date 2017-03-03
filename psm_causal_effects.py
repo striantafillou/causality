d58f6d3f9965fb730a3157859815e77ef43e86cb
sosata <sosata@gmail.com>
2017-03-03 17:00:42 -0600

    variance for repetitive data

--- psm_causal_effects.py
+++ psm_causal_effects.py
@@ -1,7 +1,7 @@
 
 # coding: utf-8
 
-# In[ ]:
+# In[4]:
 
 import numpy as np
 from sklearn import linear_model
@@ -22,10 +22,13 @@ def psm_causal_effects(treatment, outcome, confound, graph=0, scorefun='replacem
     ind_case = np.where(treatment==1)[0]
     ind_control = np.where(treatment==0)[0]
 
+    cnt_control = np.zeros(ind_control.size)
+    
     if scorefun=='unmatched':
         
         ind_matched_case = ind_case
         ind_matched_control = ind_control
+        cnt_control = np.ones(ind_control.size)
     
     else:
     
@@ -52,6 +55,9 @@ def psm_causal_effects(treatment, outcome, confound, graph=0, scorefun='replacem
 
             ind_matched_control = np.append(ind_matched_control,ind_control[ind])
             ind_matched_case = np.append(ind_matched_case,i_case)
+            
+            # counter for how many times a control is used
+            cnt_control[ind] += 1
 
             # if without replacement remove the used sample
             if scorefun=='noreplacement':
@@ -61,8 +67,13 @@ def psm_causal_effects(treatment, outcome, confound, graph=0, scorefun='replacem
                 temp.remove(ind_control[ind])
                 ind_control = np.array(temp, dtype=int)
 
-    std_pooled = np.var(outcome[ind_matched_case])*(ind_matched_case.size-1) + np.var(outcome[ind_matched_control])*(ind_matched_control.size-1)
-    std_pooled /= (ind_matched_case.size+ind_matched_control.size-2)
+    #std_pooled = np.var(outcome[ind_matched_case])*(ind_matched_case.size-1) + np.var(outcome[ind_matched_control])*(ind_matched_control.size-1)
+    #std_pooled /= (ind_matched_case.size+ind_matched_control.size-2)
+    #std_pooled = np.sqrt(std_pooled)
+    
+    # accounting for multiplicity in controls
+    ind_matched_control_uniq = np.unique(ind_matched_control)
+    std_pooled = np.var(outcome[ind_matched_case])/float(ind_matched_case.size) +        np.var(outcome[ind_matched_control_uniq])*np.sum(np.multiply(cnt_control,cnt_control))/float(np.sum(cnt_control)**2)
     std_pooled = np.sqrt(std_pooled)
     
     return (np.mean(outcome[ind_matched_case])-np.mean(outcome[ind_matched_control]))/std_pooled
--- run_psm_lags.py
+++ run_psm_lags.py
@@ -1,7 +1,7 @@
 
 # coding: utf-8
 
-# In[10]:
+# In[2]:
 
 import pickle
 import pandas as pd
@@ -14,7 +14,7 @@ with open('data.dat') as f:
     data, subjects = pickle.load(f)
 f.close()
 
-n_boot = 10
+n_boot = 100
 
 ind_set = range(len(data))
 
@@ -45,22 +45,22 @@ for k in range(n_boot):
         es_m2s_um[c] = psm_causal_effects(treatment=data_s[treatment], outcome=data_s[outcome], confound=[], scorefun='unmatched')
         
         # mood on sleep - lag 0
-        confound = ['act_prev2','daytype','stress_prev2','energy_prev2','focus_prev2']
+        confound = ['dow', 'mean_temp_prev2', 'clear_prev2', 'act_prev2','daytype','stress_prev2','energy_prev2','focus_prev2']
         data_s = extract_remove_nans(data[i], [treatment]+[outcome]+confound)
         es_m2s[c,0] = psm_causal_effects(treatment=data_s[treatment], outcome=data_s[outcome], confound=data_s[confound], scorefun='replacement')
         
         # mood on sleep - lag 1
-        confound = ['act_prev2','daytype','stress_prev2','energy_prev2','focus_prev2','mood_prev2','quality_prev']
+        confound = ['dow', 'mean_temp_prev2', 'clear_prev2', 'act_prev2','daytype','stress_prev2','energy_prev2','focus_prev2','mood_prev2','quality_prev']
         data_s = extract_remove_nans(data[i], [treatment]+[outcome]+confound)
         es_m2s[c,1] = psm_causal_effects(treatment=data_s[treatment], outcome=data_s[outcome], confound=data_s[confound], scorefun='replacement')
 
         # mood on sleep - lag 2
-        confound = ['act_prev2','daytype','stress_prev2','energy_prev2','focus_prev2','mood_prev2','quality_prev','mood_prev3','quality_prev2']
+        confound = ['dow', 'mean_temp_prev2', 'clear_prev2', 'act_prev2','daytype','stress_prev2','energy_prev2','focus_prev2','mood_prev2','quality_prev','mood_prev3','quality_prev2']
         data_s = extract_remove_nans(data[i], [treatment]+[outcome]+confound)
         es_m2s[c,2] = psm_causal_effects(treatment=data_s[treatment], outcome=data_s[outcome], confound=data_s[confound], scorefun='replacement')
 
         # mood on sleep - lag 3
-        confound = ['act_prev2','daytype','stress_prev2','energy_prev2','focus_prev2','mood_prev2','quality_prev','mood_prev3','quality_prev2','mood_prev4','quality_prev3']
+        confound = ['dow', 'mean_temp_prev2', 'clear_prev2', 'act_prev2','daytype','stress_prev2','energy_prev2','focus_prev2','mood_prev2','quality_prev','mood_prev3','quality_prev2','mood_prev4','quality_prev3']
         data_s = extract_remove_nans(data[i], [treatment]+[outcome]+confound)
         es_m2s[c,3] = psm_causal_effects(treatment=data_s[treatment], outcome=data_s[outcome], confound=data_s[confound], scorefun='replacement')
 
@@ -73,22 +73,22 @@ for k in range(n_boot):
         es_s2m_um[c] = psm_causal_effects(treatment=data_s[treatment], outcome=data_s[outcome], confound=[], scorefun='unmatched')
         
         # sleep on mood - lag 0
-        confound = ['act_prev','daytype','stress_prev','energy_prev','focus_prev']
+        confound = ['dow', 'mean_temp_prev', 'clear_prev', 'act_prev','daytype','stress_prev','energy_prev','focus_prev']
         data_s = extract_remove_nans(data[i], [treatment]+[outcome]+confound)
         es_s2m[c,0] = psm_causal_effects(treatment=data_s[treatment], outcome=data_s[outcome], confound=data_s[confound], scorefun='replacement')
         
         # sleep on mood - lag 1
-        confound = ['act_prev','daytype','stress_prev','energy_prev','focus_prev','mood_prev','quality_prev']
+        confound = ['dow', 'mean_temp_prev', 'clear_prev','act_prev','daytype','stress_prev','energy_prev','focus_prev','mood_prev','quality_prev']
         data_s = extract_remove_nans(data[i], [treatment]+[outcome]+confound)
         es_s2m[c,1] = psm_causal_effects(treatment=data_s[treatment], outcome=data_s[outcome], confound=data_s[confound], scorefun='replacement')
         
         # sleep on mood - lag 2
-        confound = ['act_prev','daytype','stress_prev','energy_prev','focus_prev','mood_prev','quality_prev','mood_prev2','quality_prev2']
+        confound = ['dow', 'mean_temp_prev', 'clear_prev','act_prev','daytype','stress_prev','energy_prev','focus_prev','mood_prev','quality_prev','mood_prev2','quality_prev2']
         data_s = extract_remove_nans(data[i], [treatment]+[outcome]+confound)
         es_s2m[c,2] = psm_causal_effects(treatment=data_s[treatment], outcome=data_s[outcome], confound=data_s[confound], scorefun='replacement')
         
         # sleep on mood - lag 3
-        confound = ['act_prev','daytype','stress_prev','energy_prev','focus_prev','mood_prev','quality_prev','mood_prev2','quality_prev2','mood_prev3','quality_prev3']
+        confound = ['dow', 'mean_temp_prev', 'clear_prev','act_prev','daytype','stress_prev','energy_prev','focus_prev','mood_prev','quality_prev','mood_prev2','quality_prev2','mood_prev3','quality_prev3']
         data_s = extract_remove_nans(data[i], [treatment]+[outcome]+confound)
         es_s2m[c,3] = psm_causal_effects(treatment=data_s[treatment], outcome=data_s[outcome], confound=data_s[confound], scorefun='replacement')
         
@@ -99,25 +99,22 @@ for k in range(n_boot):
     
 
 
-# In[ ]:
-
-es_m2s
-
-
-# In[11]:
+# In[3]:
 
 import matplotlib.pyplot as plt
+# from matplotlib import rc
+# rc('font',**{'family':'sans-serif','sans-serif':['Helvetica']})
+# rc('text', usetex=True)
 get_ipython().magic(u'matplotlib inline')
-plt.figure(figsize=(6,3))
-plt.barh([0.75,1.75,2.75,3.75,4.75],np.concatenate([np.array([np.mean(es_m2s_um_mean)]),np.mean(es_m2s_mean,axis=0)],axis=0).reshape([5,1]),xerr=np.array([    [np.mean(es_m2s_um_mean)-np.percentile(es_m2s_um_mean,2.5),np.percentile(es_m2s_um_mean,97.5)-np.mean(es_m2s_um_mean)],    [np.mean(es_m2s_mean[:,0])-np.percentile(es_m2s_mean[:,0],2.5),np.percentile(es_m2s_mean[:,0],97.5)-np.mean(es_m2s_mean[:,0])],    [np.mean(es_m2s_mean[:,1])-np.percentile(es_m2s_mean[:,1],2.5),np.percentile(es_m2s_mean[:,1],97.5)-np.mean(es_m2s_mean[:,1])],    [np.mean(es_m2s_mean[:,2])-np.percentile(es_m2s_mean[:,2],2.5),np.percentile(es_m2s_mean[:,2],97.5)-np.mean(es_m2s_mean[:,2])],    [np.mean(es_m2s_mean[:,3])-np.percentile(es_m2s_mean[:,3],2.5),np.percentile(es_m2s_mean[:,3],97.5)-np.mean(es_m2s_mean[:,3])]]).reshape(2,5),    ecolor=(0,0,0),height=.25,color=(.5,.5,1))
-plt.barh([1,2,3,4,5],np.concatenate([np.array([np.mean(es_s2m_um_mean)]),np.mean(es_s2m_mean,axis=0)],axis=0).reshape([5,1]),xerr=np.array([    [np.mean(es_s2m_um_mean)-np.percentile(es_s2m_um_mean,2.5),np.percentile(es_s2m_um_mean,97.5)-np.mean(es_s2m_um_mean)],    [np.mean(es_s2m_mean[:,0])-np.percentile(es_s2m_mean[:,0],2.5),np.percentile(es_s2m_mean[:,0],97.5)-np.mean(es_s2m_mean[:,0])],    [np.mean(es_s2m_mean[:,1])-np.percentile(es_s2m_mean[:,1],2.5),np.percentile(es_s2m_mean[:,1],97.5)-np.mean(es_s2m_mean[:,1])],    [np.mean(es_s2m_mean[:,2])-np.percentile(es_s2m_mean[:,2],2.5),np.percentile(es_s2m_mean[:,2],97.5)-np.mean(es_s2m_mean[:,2])],    [np.mean(es_s2m_mean[:,3])-np.percentile(es_s2m_mean[:,3],2.5),np.percentile(es_s2m_mean[:,3],97.5)-np.mean(es_s2m_mean[:,3])]]).reshape(2,5),    ecolor=(0,0,0),height=.25,color=(.5,1,.5))
-plt.xlim([0,1])
-plt.yticks([1,2,3,4,5],['unmatched', 'lag 0','lag 0,1','lag 0,1,2','lag 0,1,2,3'],rotation=0);
-plt.xlabel('Mean Personal Causal Effect')
-plt.legend(['Mood on Sleep Quality','Sleep Quality on Mood'],loc='upper right',bbox_to_anchor=(1.3, 1.05), fontsize=10);
 
-
-# In[ ]:
-
-np.concatenate([np.array([np.mean(es_m2s_um_mean)]),np.mean(es_m2s_mean,axis=0)],axis=0).reshape([5,1])
+plt.figure(figsize=(6,5))
+plt.bar([-.25,1.75,2.75,3.75,4.75],np.concatenate([np.array([np.mean(es_m2s_um_mean)]),np.mean(es_m2s_mean,axis=0)],axis=0).reshape([5,1]),yerr=np.array([    [np.mean(es_m2s_um_mean)-np.percentile(es_m2s_um_mean,2.5),np.percentile(es_m2s_um_mean,97.5)-np.mean(es_m2s_um_mean)],    [np.mean(es_m2s_mean[:,0])-np.percentile(es_m2s_mean[:,0],2.5),np.percentile(es_m2s_mean[:,0],97.5)-np.mean(es_m2s_mean[:,0])],    [np.mean(es_m2s_mean[:,1])-np.percentile(es_m2s_mean[:,1],2.5),np.percentile(es_m2s_mean[:,1],97.5)-np.mean(es_m2s_mean[:,1])],    [np.mean(es_m2s_mean[:,2])-np.percentile(es_m2s_mean[:,2],2.5),np.percentile(es_m2s_mean[:,2],97.5)-np.mean(es_m2s_mean[:,2])],    [np.mean(es_m2s_mean[:,3])-np.percentile(es_m2s_mean[:,3],2.5),np.percentile(es_m2s_mean[:,3],97.5)-np.mean(es_m2s_mean[:,3])]]).reshape(2,5),    ecolor=(0,0,0),width=.25,color=(.5,.5,.8))
+plt.bar([0,2,3,4,5],np.concatenate([np.array([np.mean(es_s2m_um_mean)]),np.mean(es_s2m_mean,axis=0)],axis=0).reshape([5,1]),yerr=np.array([    [np.mean(es_s2m_um_mean)-np.percentile(es_s2m_um_mean,2.5),np.percentile(es_s2m_um_mean,97.5)-np.mean(es_s2m_um_mean)],    [np.mean(es_s2m_mean[:,0])-np.percentile(es_s2m_mean[:,0],2.5),np.percentile(es_s2m_mean[:,0],97.5)-np.mean(es_s2m_mean[:,0])],    [np.mean(es_s2m_mean[:,1])-np.percentile(es_s2m_mean[:,1],2.5),np.percentile(es_s2m_mean[:,1],97.5)-np.mean(es_s2m_mean[:,1])],    [np.mean(es_s2m_mean[:,2])-np.percentile(es_s2m_mean[:,2],2.5),np.percentile(es_s2m_mean[:,2],97.5)-np.mean(es_s2m_mean[:,2])],    [np.mean(es_s2m_mean[:,3])-np.percentile(es_s2m_mean[:,3],2.5),np.percentile(es_s2m_mean[:,3],97.5)-np.mean(es_s2m_mean[:,3])]]).reshape(2,5),    ecolor=(0,0,0),width=.25,color=(.5,.8,.5))
+a = plt.ylim([0,2.5])
+plt.xlim([-.5,5.5])
+plt.xticks([0,2,3,4,5],['unmatched', 'T=0','T=1','T=2','T=3'],rotation=0);
+plt.ylabel('Mean Personal Causal Effect',fontsize=14)
+plt.xlabel('lags 0 to T',fontsize=14)
+plt.legend(['Mood on Sleep Quality','Sleep Quality on Mood'],loc='upper right',bbox_to_anchor=(1, 1), fontsize=10);
+plt.plot([1,1],[a[0],a[1]],'--',color=(0,0,0))
 
